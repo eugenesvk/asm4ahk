37066dfea092a9092ec10e336b625900b11db024
eugenesvk <eugenesvk@users.noreply.github.com>
2023-09-12 23:40:43 +0700

    comment out ret42

--- src/asm4ahk.bin.rs
+++ src/asm4ahk.bin.rs
@@ -1,8 +1,8 @@
-use asm4ahk_lib::ret42;
+// use asm4ahk_lib::ret42;
 use asm4ahk_lib::add;
 
 fn main() {
-  let lib42:i32 = ret42();
+  // let lib42:i32 = ret42();
   let libadd:usize = add(1,2);
   println!("Hello, world! lib42{} libadd{}", lib42,libadd);
 }
