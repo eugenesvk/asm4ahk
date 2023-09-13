6a45d99e9e8072de991511d9c7a704020ffcbce9
eugenesvk <eugenesvk@users.noreply.github.com>
2023-09-13 17:44:41 +0700

    update bin

--- src/asm4ahk.bin.rs
+++ src/asm4ahk.bin.rs
@@ -1,8 +1,7 @@
-// use asm4ahk_lib::ret42;
-use asm4ahk_lib::add;
+#![allow(unused_imports)]
+use asm4ahk_lib::{add};
 
 fn main() {
-  // let lib42:i32 = ret42();
   let libadd:usize = add(1,2);
   println!("Hello, world! lib42{} libadd{}", lib42,libadd);
 }
