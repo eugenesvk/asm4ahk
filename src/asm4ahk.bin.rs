1b2368a627cec828209a14aaca472a99fe61d342
eugenesvk <eugenesvk@users.noreply.github.com>
2023-09-15 22:41:57 +0700

    update lib with bin functions

--- src/asm4ahk.bin.rs
+++ src/asm4ahk.bin.rs
@@ -1,7 +1,11 @@
 #![allow(unused_imports)]
-use asm4ahk_lib::{add};
+use asm4ahk_lib::{add,ret42,add_ext,ret42_ext};
 
 fn main() {
   let libadd:usize = add(1,2);
-  println!("Hello, world! lib42{} libadd{}", lib42,libadd);
+  let libadd_ext:usize = add_ext(1,2);
+  let libret42:i32 = ret42();
+  let libret42_ext:i32 = ret42_ext();
+  println!("Hello, world! libret42{} libadd{} libadd_ext{} libret42_ext{}"
+   ,                      libret42  ,libadd   ,libadd_ext  ,libret42_ext);
 }
