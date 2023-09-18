1fe8a2503fa33e2407fa1a494426cfb1047bcd5e
eugenesvk <eugenesvk@users.noreply.github.com>
2023-09-19 00:27:30 +0700

    update bin

--- src/asm4ahk.bin.rs
+++ src/asm4ahk.bin.rs
@@ -1,4 +1,10 @@
-#![allow(unused_imports)]
+#![allow(unused_imports,unused_variables,unreachable_code,dead_code,non_upper_case_globals,unused_mut)]
+extern crate helper;
+use helper        	::*; // gets macros
+use helper::alias 	::*;
+use helper::helper	::*;
+use helper::parser	::*;
+
 use asm4ahk_lib::{add,ret42,add_ext,ret42_ext};
 
 fn main() {
@@ -6,6 +12,4 @@ fn main() {
   let libadd_ext:usize = add_ext(1,2);
   let libret42:i32 = ret42();
   let libret42_ext:i32 = ret42_ext();
-  println!("Hello, world! libret42{} libadd{} libadd_ext{} libret42_ext{}"
-   ,                      libret42  ,libadd   ,libadd_ext  ,libret42_ext);
 }
