14ada4900dd2fca74432e255f2b67cf9a1129574
eugenesvk <eugenesvk@users.noreply.github.com>
2023-09-21 03:47:22 +0700

    add build script

---
+++ build.rs
@@ -0,0 +1,10 @@
+#![allow(non_snake_case,non_upper_case_globals,non_camel_case_types,unused_imports,unused_mut,unused_variables,dead_code,unused_assignments,unused_macros)]
+
+extern crate helper;
+use helper        	::*; // gets macros
+use helper::alias 	::*;
+use helper::helper	::*;
+use helper::parser	::*;
+
+fn main() {
+}
